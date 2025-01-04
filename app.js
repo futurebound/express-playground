e82a7059b8ba5d0c019aeeb4ebac89350ee355e7
futurebound <mitchellm1618@gmail.com>
2025-01-04 11:04:31 -0800

    feat: global error handling with custom error capability

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ const app = express()
 const authorRouter = require('./routes/authorRouter')
 
 app.use('/authors', authorRouter)
-// app.use('/')
+
+// Every thrown error in the application or the previous middleware function
+//  calling `next` with an error as an argument will eventually go to this
+//  middleware function
+app.use((err, req, res, next) => {
+  console.error(err)
+  res.status(err.statusCode || 500).send(err.message)
+})
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
