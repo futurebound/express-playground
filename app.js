6fdf7bb389e180734dec0c8d5837fc65758f8eb3
futurebound <mitchellm1618@gmail.com>
2025-01-05 10:15:45 -0800

    feat: form to create new users
    added storage, route, controller for users
    
    added ejs templates for index, create and update user, and errors

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const express = require('express')
 const app = express()
 
 const authorRouter = require('./routes/authorRouter')
+const usersRouter = require('./routes/usersRouter')
 
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true }))
+app.use('/', usersRouter)
 app.use('/authors', authorRouter)
 
 // Every thrown error in the application or the previous middleware function
