155c76b4d653a06b64286e6815d1c1837572a89a
futurebound <mitchellm1618@gmail.com>
2025-01-05 10:21:34 -0800

    feat: update users
    add update functions to usersController
    
    add update usersRoutes
    
    add update button to index.ejs
    
    fix name conflict in errors partial

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -3,7 +3,13 @@ const usersController = require('../controllers/usersController')
 const usersRouter = Router()
 
 usersRouter.get('/', usersController.usersListGet)
+
+// Create Users routes
 usersRouter.get('/create', usersController.usersCreateGet)
 usersRouter.post('/create', usersController.usersCreatePost)
+
+// Update Users routes
+usersRouter.get('/:id/update', usersController.usersUpdateGet)
+usersRouter.post('/:id/update', usersController.usersUpdatePost)
 
 module.exports = usersRouter
